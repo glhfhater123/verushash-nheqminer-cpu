71760702ef7be725c40fe7ce6f617322fe9b29ce
glhfhater123 <52426076+glhfhater123@users.noreply.github.com>
2019-07-10 22:44:53 +0200

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+PoolHost=
+Port=
+PublicVerusCoinAddress=
+WorkerName=
+Threads=
+#set working directory to the location of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+./nheqminer -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"
